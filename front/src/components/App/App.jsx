e9cc427f71f535779d5855d4ff9a31de44e76e90
AndreuV12 <andreuvillaro12@gmail.com>
2023-01-19 00:49:40 +0100

    some small changes

--- front/src/components/App/App.jsx
+++ front/src/components/App/App.jsx
@@ -1,40 +1,20 @@
 import "./App.css"
 import {Routes, Route } from 'react-router-dom'
-import initialOp from "../../helpers/initialOp"
-import Navbar from "../UI/Navbar/Navbar"
-import InputBar from "../UI/InputBar/InputBar"
+
 import OpSelector from "../OpSelector/OpSelector"
 import OpeningEditor from "../OpeningEditor/OpeningEditor"
 const App = () => (
     
     <div className="App">  
-        {/* <Navbar links = {[
-            {   link: "/",
-                name: "HOME" },
-            {   link: "/openings",
-                name: "APERTURES" },
-            {   link: "/board",
-                name: "TABLERO" },
-        ]}/> */}
-        
         <Routes>
             <Route path='/' element = {
-                <InputBar placeholder={"Filter by name"} onSubmit={(input)=>{console.log(input)}}></InputBar>    
+                <OpSelector/>
             }
             />
-
-            <Route path='/openings' element = {
-                <OpSelector/>
-            }/>
 
             <Route path='/openings/:opening_id' element = {
                 <OpeningEditor/>
             }/>
-
-            <Route path='/openings/edit/' element = {
-                <h1>OpeningEditor</h1>
-            }/>
-
         </Routes>
     </div>
 )
